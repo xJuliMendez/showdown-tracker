ca9b271d3a6e7ebdf8e1aba63b66e47dbb32b4fe
Julian Mendez <jmendezibarra@gmail.com>
2025-04-12 14:13:04 +0200

    initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxt/eslint'],
   css: ['~/app/assets/css/main.css'],
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  }
 })
